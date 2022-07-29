f5eab426a8771fa1fd316d01365ec161bef181a8
Kian-Tat Lim <ktl@slac.stanford.edu>
2022-07-29 09:31:43 -0700

    Log parse errors.

--- python/exposure_info.py
+++ python/exposure_info.py
@@ -22,33 +22,37 @@ class ExposureInfo:
     detector_name_in_raft: str
 
     def __init__(self, path):
-        if path.startswith("s3://"):
-            path = path[len("s3://"):]
-        self.path = path
-        (
-            self.bucket,
-            self.instrument,
-            obs_day,
-            self.exp_id,
-            self.filename,
-        ) = path.split("/")
-        (
-            instrument_code,
-            controller,
-            obs_day2,
-            seq_num,
-        ) = exp_id.split("_")
-        (
-            self.instrument_code,
-            self.controller,
-            self.obs_day,
-            self.seq_num,
-            self.detector_raft_name,
-            self.detector_name_in_raft,
-        ) = self.filename.split("_")
-        self.detector_full_name = (f"{self.detector_raft_name}"
-            f"_{self.detector_name_in_raft}")
-        if obs_day != self.obs_day or obs_day2 != self.obs_day:
-            log.warn(f"Mismatched observation dates: {path}")
-        if seq_num != self.seq_num:
-            log.warn(f"Mismatched sequence numbers: {path}")
+        try:
+            if path.startswith("s3://"):
+                path = path[len("s3://"):]
+            self.path = path
+            (
+                self.bucket,
+                self.instrument,
+                obs_day,
+                self.exp_id,
+                self.filename,
+            ) = path.split("/")
+            (
+                instrument_code,
+                controller,
+                obs_day2,
+                seq_num,
+            ) = self.exp_id.split("_")
+            (
+                self.instrument_code,
+                self.controller,
+                self.obs_day,
+                self.seq_num,
+                self.detector_raft_name,
+                self.detector_name_in_raft,
+            ) = self.filename.split("_")
+            self.detector_full_name = (f"{self.detector_raft_name}"
+                f"_{self.detector_name_in_raft}")
+            if obs_day != self.obs_day or obs_day2 != self.obs_day:
+                log.warn(f"Mismatched observation dates: {path}")
+            if seq_num != self.seq_num:
+                log.warn(f"Mismatched sequence numbers: {path}")
+        except Exception as e:
+            log.error(f"Unable to parse: {path}")
+            raise
